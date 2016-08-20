94519bf49a92fb9e4ed6ded63adf2ca3b3a56cbc
dotgeng <15093248695@163.com>
2016-08-20 15:21:37 +0800

    商品列表实现完毕

--- taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
+++ taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
@@ -6,6 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
+import com.fasterxml.jackson.core.util.TextBuffer;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.taotao.common.pojo.EUDataGridResult;
 import com.taotao.mapper.TbItemMapper;
 import com.taotao.pojo.TbItem;
 import com.taotao.pojo.TbItemExample;
@@ -28,6 +32,19 @@ public class ItemServiceImpl implements ItemService{
 			return list.get(0);
 		}
 		return null;
+	}
+	@Override
+	public EUDataGridResult getItemList(int page, int rows) {
+		//查询商品列表
+		TbItemExample example = new TbItemExample();
+		//分页
+		PageHelper.startPage(page,rows);
+		List<TbItem> list = itemMapper.selectByExample(example);
+		PageInfo<TbItem> pageInfo = new PageInfo<>(list);
+		EUDataGridResult result = new EUDataGridResult();
+		result.setRows(list);
+		result.setTotal(pageInfo.getTotal());
+		return result;
 	}
 
 }
